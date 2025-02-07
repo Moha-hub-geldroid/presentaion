71f2a5927476e37e5177f64c998156317ee344e9
mohammad <81656341+Moha-hub-geldroid@users.noreply.github.com>
2025-02-07 16:17:53 +0300

    Update forms.py

--- presentaion/forms.py
+++ presentaion/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,ValidationError
 from flask_wtf.file import FileField, FileAllowed
-from presentation.models import User
+from presentaion.models import User
 from flask_login import current_user
 
 class LoginForm(FlaskForm):
